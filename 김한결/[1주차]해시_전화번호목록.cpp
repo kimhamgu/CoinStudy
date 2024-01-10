98f19355d0505c977685bb6dcc4fb85164425557
mangruf <123349413+mangruf0927@users.noreply.github.com>
2024-01-10 11:58:04 +0900

    1주차 2번

---
+++ 김한결/[1주차]해시_전화번호목록.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) 
+{
+    sort(phone_book.begin(), phone_book.end());
+
+    for (int i = 0; i < phone_book.size() - 1; i++)
+    {
+        //정렬 했으니까 다음 번호랑만 비교해주면 됨
+        if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size()))
+        {
+            return false;
+        }
+    }
+    return true;
+}
